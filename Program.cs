d018bf527c0b2f78d021cbc44cc22d4a2c0a1dd6
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-25 01:58:37 -0300

    Finalização do programa

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        Console.Clear();
+        
+        const Double g = 9.80665;
+
+        Double anguloGraus , velocidade , anguloRadianos , alcance , altura;
+
+        Console.WriteLine("---------------");
+        Console.WriteLine("-- Projétil --");
+        Console.WriteLine("---------------\n");
+
+        Console.Write("Entre com a velocidade, em m/s..: ");
+        velocidade = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine();
+
+        Console.Write("Entre com o ângulo, em graus....: ");
+        anguloGraus = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine();
+
+        anguloRadianos = anguloGraus * (Math.PI/180);
+
+        alcance = (Math.Pow(velocidade, 2) * Math.Sin(2 * anguloRadianos)) / g;
+
+        altura = Math.Pow((velocidade * Math.Sin(anguloRadianos)), 2) / (2 * g);
+
+        Console.Write($"Alcance........: {alcance:N2}\n");
+        
+        Console.WriteLine();
+        
+        Console.Write($"Altura máxima..: {altura:N2}");
+
+        }
+    }
+}
